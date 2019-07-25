37d9ef8067af82b62ce0097ae55fe2e004eb6114
Sema <Danteh.Sema@baltimorecity.gov>
2019-07-24 20:30:30 -0400

    building forms apps for real lol

---
+++ project/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
